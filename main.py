ee59bfe02ce56dab11aa4b46e92ad53436391139
Talia Kaplanian <tkaplani@redhat.com>
2020-06-24 09:07:04 -0400

    Changed the return in main.py to render about.html template

--- main.py
+++ main.py
@@ -6,9 +6,9 @@ app = Flask(__name__)
 def home():
 	return render_template("home.html")
 
-@app.route("/talia")
-def talia():
-	return "Hello, Talia!"
+@app.route("/about")
+def about():
+	return render_template("about.html")
 
 if __name__ == "__main__":
 	app.run(debug=True)

446e50dbb012c866c588910efa447d4d26c3838d
Talia Kaplanian <tkaplani@redhat.com>
2020-06-26 10:43:35 -0400

    Added getData/GET example

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template
+import requests
+import json
 
 app = Flask(__name__)
 
@@ -9,6 +11,15 @@ def home():
 @app.route("/about")
 def about():
 	return render_template("about.html")
+
+@app.route("/getData")
+def getData():
+	response = requests.get('https://cat-fact.herokuapp.com/facts')
+	if response.status_code != 200:
+		raise ApiError('GET /tasks/ {}'.format[response.status_code])
+	respObj = json.loads(response.text)
+	print(respObj["all"][1])
+	return dict(respObj["all"][1])
 
 if __name__ == "__main__":
 	app.run(debug=True)

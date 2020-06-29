d35088195ff3a00579b4afcb29eae789438251d7
Talia Kaplanian <tkaplani@redhat.com>
2020-06-29 13:22:38 -0400

    Cleaned code

--- main.py
+++ main.py
@@ -5,22 +5,27 @@ import random
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def home():
-	return render_template("home.html")
+    return render_template("home.html")
+
 
 @app.route("/about")
 def about():
-	return render_template("about.html")
+    return render_template("about.html")
+
 
 @app.route("/getData")
 def getData():
-	response = requests.get('https://cat-fact.herokuapp.com/facts')
-	#if response.status_code != 200:
-		#raise ApiError('GET /tasks/ {}'.format[response.status_code])
-	respObj = json.loads(response.text)
-	print(respObj["all"][1]["text"])
-	return respObj["all"][random.randint(0, 233)]["text"]
+    response = requests.get('https://cat-fact.herokuapp.com/facts')
+    print(response)
+    #if response.status_code != 200:
+        #raise ApiError('GET /tasks/ {}'.format[response.status_code])
+    respObj = json.loads(response.text)
+    text = respObj["all"][random.randint(0, 233)]["text"]
+    return text
+
 
 if __name__ == "__main__":
-	app.run(debug=True)
+    app.run(debug=True)
